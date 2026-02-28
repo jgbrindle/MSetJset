d3ef2ae45e2c13e7632b718279512578ff5aff37
Jeff Brindle <30170199+jgbrindle@users.noreply.github.com>
2026-02-28 15:53:01 +1100

    Initial Commit

---
+++ MSetJset/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  MSetJset
+//
+//  Created by Jeff Brindle on 28/2/2026.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
